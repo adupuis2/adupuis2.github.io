be4a3e7a9d8d08a8391bb48ad132e259f307cc79
Alain Dupuis <adupuis2@unb.ca>
2018-10-01 02:03:11 -0300

    Cleaning files for starter kit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import logo from './images/logo.svg';
-import appStyles from './appStyles';
-import Header from './components/Header/';
 import {createMuiTheme, Grid, MuiThemeProvider} from '@material-ui/core';
 
 const appTheme = createMuiTheme({
   palette: {
-    type: "dark",
+    type: "light",
   }
 });
 
