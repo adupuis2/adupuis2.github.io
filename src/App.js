c96b325cc1170f969318a8b6fede760e1acf1499
Alain Dupuis <adupuis2@unb.ca>
2018-09-30 23:10:40 -0300

    Updating .gitignore Testing materialui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './images/logo.svg';
 import './App.css';
+import {Grid }from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Grid container xs={12} className="App">
+        <Grid item sx={12}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -14,7 +16,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
           Testing webpack dev server.
         </p>
-      </div>
+        </Grid>
+      </Grid>
     );
   }
 }
