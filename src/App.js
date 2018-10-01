d6606cfe74705925c436b92c48e6f2b07f90c4b6
Alain Dupuis <adupuis2@unb.ca>
2018-10-01 01:21:12 -0300

    Getting the ball rolling!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import logo from './images/logo.svg';
-import './App.css';
-import {Grid }from '@material-ui/core';
+import appStyles from './appStyles';
+import Header from './components/Header/';
+import {createMuiTheme, Grid, MuiThemeProvider} from '@material-ui/core';
+
+const appTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <Grid container xs={12} className="App">
-        <Grid item sx={12}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          Testing webpack dev server.
-        </p>
+      <MuiThemeProvider theme={appTheme}>
+        <Grid container className="App">
+          <Header />
         </Grid>
-      </Grid>
+      </MuiThemeProvider>
     );
   }
 }
