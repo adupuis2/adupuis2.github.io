8671c04432093cb6093546383c629196d6916a61
Alain Dupuis <adupuis2@unb.ca>
2018-10-14 00:31:59 -0300

    Merge branch 'working' into starter-kit
    # Conflicts:
    #	src/App.js
    #	src/components/common/Header/index.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import Header from './components/common/Header/';
-import {createMuiTheme, Grid, MuiThemeProvider} from '@material-ui/core';
-import lightBlue from '@material-ui/core/colors/lightBlue';
-import pink from '@material-ui/core/colors/pink';
-import red from '@material-ui/core/colors/red';
-import {hot} from 'react-hot-loader';
+import React, { Component } from "react";
+import Header from "./components/common/Header/";
+import { createMuiTheme, Grid, MuiThemeProvider } from "@material-ui/core";
+import lightBlue from "@material-ui/core/colors/lightBlue";
+import pink from "@material-ui/core/colors/pink";
+import red from "@material-ui/core/colors/red";
+import { hot } from "react-hot-loader";
 
 const appTheme = createMuiTheme({
   typography: {
-    useNextVariants: true,
+    useNextVariants: true
   },
   palette: {
-    // type: "light",
     primary: lightBlue,
     secondary: pink,
     error: red,
-    // contrastThreshold: 10,
-    // tonalOffset: .5,
+    contrastThreshold: 5,
+    tonalOffset: 0.5
   }
 });
 
@@ -24,9 +23,7 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={appTheme}>
-        <Grid>
-          <Header />
-        </Grid>
+        <Header />
       </MuiThemeProvider>
     );
   }
--- src/appStyles.js
+++ src/appStyles.js
@@ -1,30 +1,30 @@
-import {createStyles} from '@material-ui/core';
+import { createStyles } from "@material-ui/core";
 
-const appStyles = () => createStyles({
-  app: {
-    textAlign: 'center',
-  },
+const appStyles = () =>
+  createStyles({
+    app: {
+      textAlign: "center"
+    },
 
+    appLogo: {
+      animation: "App-logo-spin infinite 20s linear",
+      height: "80px"
+    },
 
-  appLogo: {
-    animation: 'App-logo-spin infinite 20s linear',
-    height: '80px',
-  },
+    appHeader: {
+      backgroundColor: "#222",
+      height: "150px",
+      padding: "20px",
+      color: "white"
+    },
 
-  appHeader: {
-    backgroundColor: '#222',
-    height: '150px',
-    padding: '20px',
-    color: 'white',
-  },
+    appTitle: {
+      fontSize: "1.5em"
+    },
 
-  appTitle: {
-    fontSize: '1.5em',
-  },
-
-  appIntro: {
-    fontSize: 'large',
-  },
-});
+    appIntro: {
+      fontSize: "large"
+    }
+  });
 
 export default appStyles;
